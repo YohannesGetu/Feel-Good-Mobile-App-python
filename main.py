f5bf24451ec38c939acf0f2921a7720130bdc638
Yohannes Getu <nede.evoli@gmail.com>
2021-06-08 16:38:01 +0300

    added frontend design for sign up page

--- main.py
+++ main.py
@@ -12,10 +12,16 @@ Builder.load_file('design.kv')
 
 
 class LoginScreen(Screen):
-    pass
+
+    def sign_up(self):
+        self.manager.current = "sign_up_screen"
 
 
 class RootWidget(ScreenManager):
+    pass
+
+
+class SignUpScreen(Screen):
     pass
 
 
