4c1cf01b87fdc32015e4bd0a23fd909f9fbd378d
Yohannes Getu <nede.evoli@gmail.com>
2021-06-11 14:17:17 +0300

    implemented login page backend

--- main.py
+++ main.py
@@ -17,7 +17,13 @@ class LoginScreen(Screen):
 
     def sign_up(self):
         self.manager.current = "sign_up_screen"
-
+    def login(self, uname, pword):
+        with open("users.json") as file:
+            users = json.load(file)
+        if uname in users and users[uname]['password'] == pword:
+            self.manager.current = "login_screen_success"
+        else:
+            self.ids.login_wrong.text = "Wrong username or password!"
 
 class RootWidget(ScreenManager):
     pass
@@ -40,6 +46,11 @@ class SignUpScreenSuccess(Screen):
         self.manager.transition.direction = 'right'
         self.manager.current = "login_screen"
 
+
+class LoginScreenSuccess(Screen):
+    def log_out(self):
+        self.manager.transition.direction = 'right'
+        self.manager.current = "login_screen"
 
 class MainApp(App):
     def build(self):
