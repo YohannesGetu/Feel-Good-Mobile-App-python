00fc5bd8120be43fdbd3bebfcccc3a9c6596235b
Yohannes Getu <nede.evoli@gmail.com>
2021-06-12 09:37:18 +0300

    implemented display quote to user

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 import json
+import glob
+import random
 from datetime import datetime
+from pathlib import Path
 
 from kivy import Config
 Config.set('graphics', 'multisamples', '0')
@@ -17,6 +20,7 @@ class LoginScreen(Screen):
 
     def sign_up(self):
         self.manager.current = "sign_up_screen"
+
     def login(self, uname, pword):
         with open("users.json") as file:
             users = json.load(file)
@@ -24,6 +28,7 @@ class LoginScreen(Screen):
             self.manager.current = "login_screen_success"
         else:
             self.ids.login_wrong.text = "Wrong username or password!"
+
 
 class RootWidget(ScreenManager):
     pass
@@ -51,6 +56,18 @@ class LoginScreenSuccess(Screen):
     def log_out(self):
         self.manager.transition.direction = 'right'
         self.manager.current = "login_screen"
+
+    def get_quote(self, feel):
+        feel = feel.lower()
+        available_feelings = glob.glob("quotes/*txt")
+        available_feelings = [
+            Path(filename).stem for filename in available_feelings]
+        if feel in available_feelings:
+            with open(f"quotes/{feel}.txt") as file:
+                quotes = file.readlines()
+                self.ids.quote.text = random.choice(quotes)
+        else:
+            self.ids.quote.text = "Try another feeling."
 
 class MainApp(App):
     def build(self):
